5a002e9e41fec68c3b241d846f323d10caed876c
Bobby Brace <bob.j.brace@gmail.com>
2022-03-02 16:23:20 -0600

    Began work on attack and drag functions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,10 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     float vertical;
     float moveLimiter = 0.7f;
+    bool dragging = false;
+    bool draggingHorizontal;
 
+    public GameObject arm;
     public float moveSpeed = 5.0f;
 
     // Start is called before the first frame update
@@ -26,9 +29,47 @@ public class PlayerController : MonoBehaviour
         //Set horizontal and vertical movement values
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        //Checks to see if the player is dragging something
+        if(Input.GetButton("Drag"))
+        {
+            dragging = true;
+            Drag();
+        }
+        else dragging = false;
+
+        //Checks for attack
+        if(Input.GetButtonDown("Fire1") && !dragging)
+        {
+            Attack();
+        }
     }
 
     private void FixedUpdate() 
+    {
+        if(dragging) DragMove();
+        else NormalMove();
+    }
+
+    private bool CheckForFlip()
+    {
+        if(horizontal < 0 && sprite.flipX == true) return true;
+        else if(horizontal > 0 && sprite.flipX == false) return true;
+        else return false;
+    }
+
+    
+    private void Drag()
+    {
+        dragging = true;
+    }
+
+    private void Attack()
+    {
+        
+    }
+
+    private void NormalMove()
     {
         //Check for diagonal movement
         if(horizontal !=0 && vertical !=0)
@@ -42,10 +83,8 @@ public class PlayerController : MonoBehaviour
         if(CheckForFlip()) sprite.flipX = !sprite.flipX;
     }
 
-    private bool CheckForFlip()
+    private void DragMove()
     {
-        if(horizontal < 0 && sprite.flipX == true) return true;
-        else if(horizontal > 0 && sprite.flipX == false) return true;
-        else return false;
+
     }
 }
